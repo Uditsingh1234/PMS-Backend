768bba9e885831d3efe6a2adafcf3ff19772bed3
Udit Singh <udit6706@gmail.com>
2024-07-31 18:47:44 +0530

    upadate

--- index.js
+++ index.js
@@ -1,29 +1,20 @@
+// index.js (Backend)
 import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
 import cors from 'cors';
 import { createClient } from '@supabase/supabase-js';
-
 const app = express();
 app.use(express.json());
-
-// CORS configuration
-const corsOptions = {
-  origin: 'https://pms-frontend-rflyt7y5h-udit-singhs-projects.vercel.app/', // Replace with your actual frontend URL
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-  optionsSuccessStatus: 204,
-  preflightContinue: false
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 
 // Supabase client setup
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = process.env.VITE_SUPABASE_ANON_KEY;
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 app.post('/login', async (req, res) => {
+  
   const { email, password } = req.body;
 
   try {
